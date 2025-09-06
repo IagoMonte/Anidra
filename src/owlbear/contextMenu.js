bb75ec2d159ea91deb6ebe47e37ea085819debbb
iagodt <iagomonte.dev@gmail.com>
2025-09-06 17:18:43 -0300

    add context menu

--- src/owlbear/contextMenu.js
+++ src/owlbear/contextMenu.js
@@ -1,6 +1,20 @@
 import OBR from "@owlbear-rodeo/sdk";
+const ID = "com.anidra.addto"
 
 export function setupCounter() {
-  OBR.notification.show(`Ready!!`);
-  
+  OBR.notification.show(`Anidra pronto!!`);
+  OBR.contextMenu.create({
+    id: `${ID}/context-menu`,
+    icons: [
+      {
+        icon: "/src/assets/img/add.svg",
+        label: "Adicionar ao AnidraCtl",
+        filter: {
+          every: [{ key: "layer", value: "CHARACTER" }],
+        },
+      },
+    ],
+    onClick() {
+    },
+  })
 }
