410a491a0d784d290e2a710b274243781bfa9947
iagodt <iagomonte.dev@gmail.com>
2025-09-06 16:12:44 -0300

    contextmenu Teste

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import OBR from "@owlbear-rodeo/sdk";
+import { setupContextMenu } from './owlbear/contextMenu';
+
+
 
 createApp(App).mount('#app')
+
+OBR.onReady(() => {
+  setupContextMenu();
+});
